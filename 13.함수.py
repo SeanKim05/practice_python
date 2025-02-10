e33ecc631767c0c2ca786d9435f2ef6d060cd4b2
SeanKim05 <dbgus20609@gmail.com>
2025-02-10 15:09:17 +0900

    함수&문자열

---
+++ 13.함수.py
@@ -0,0 +1,18 @@
+# 반복되는 코드 감소
+# 재사용성 향상
+# 수정이 쉬움
+
+
+# def print_msg(name):
+#     print(f"안녕하세요 {name}님")
+
+
+# print_msg("김")
+
+
+def add_num(a, b):
+    res = a + b
+    return res
+
+
+add_num(10, 20)
---
+++ 14.문자열함수.py
@@ -0,0 +1,13 @@
+# count,find,replace,split
+
+name = "sdsadaszwqz"
+print(name.find("z"))
+
+text = ["mon", "fri", "sun"]
+
+print("".join(text))
+
+text1 = "    공백    "
+text1.strip()
+text1.lstrip()
+text1.rstrip()
